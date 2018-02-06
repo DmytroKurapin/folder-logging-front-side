ecdd5d215b07e3e96b356ca16ec9f56ff01d5278
Dmitriy Kurapin <dmitriy@innitel.com>
2018-02-06 18:56:03 +0200

    Added Auth Guard

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -8,14 +8,11 @@ export class AuthGuardService implements CanActivate {
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-   /* if (this.authService.signedUser && !this.authService.signedUser.isAnonymous && this.authService.isAccepted) {
-      return true;
+    if (!localStorage.getItem('isSignedIn') || localStorage.getItem('isSignedIn') !== 'true') {
+      this.router.navigate(['/login']);
+      return false;
     }
-
-    this.authService.logout();
-    this.router.navigate(['/login'], { queryParams: {returnUrl: state.url}});
-    return false;*/
-   return true;
+    return true;
   }
 
 }
